ae26b235db42c686726df195f95cf59d46631a26
ASHISH KUMAt <akumaraman966@gmail.com>
2022-09-14 15:33:11 +0530

    update
    update

--- js/main.js
+++ js/main.js
@@ -1,64 +1,26 @@
 
-// Lets creat a required element 
+// let junrate randum number 
 
+ const imageUrl ="https://picsum.photos/200/300?random="; 
 
-//  const row = document.createElement("div");
-//  row.setAttribute("class" ,"row");
- 
-//   const img = document.createElement("img");
-//       img.setAttribute("src" , "#");
-
-//       row.append(img)
-
-//   const h2 = document.createElement('h2'); 
-//    h2.innerText="hello world";
- 
-//    row.append(h2)
-
-//    const p = document.createElement('p'); 
-//    p.innerText="this is a paragraf tax";
- 
-//    row.append(p)
-
-//===========Feact Api to get data ==============//
-
- 
-  const key ='563492ad6f91700001000001c476761ea0244c6a886db3ff26237130';
-
-
-  function getImage(){
-
-       fetch( "https://api.pexels.com/v1/curated?per_page=1", {
-            method : "GET" ,
-            He
-
-       })
-            
-  }
-    
-      
-
-
- 
-   for(let i=0; i<5; i++){
-
+   for(let i=0; i<100; i++){
+      let url =imageUrl +i;
       const row = document.createElement("div");
       row.setAttribute("class" ,"row");
       
        const img = document.createElement("img");
-           img.setAttribute("src" , "#");
-     
+           img.setAttribute("src" , url);
            row.append(img)
      
        const h2 = document.createElement('h2'); 
-        h2.innerText=`hello world ${i}`;
+        h2.innerText=`Image number ${i+1}`;
       
         row.append(h2)
-     
-        const p = document.createElement('p'); 
-        p.innerText="this is a paragraf tax";
+     // We can use this to add image discription;
+      //   const p = document.createElement('p'); 
+      //   p.innerText="this is a paragraf tax";
       
-        row.append(p)
+      //   row.append(p)
 
       document.getElementById('main').appendChild(row);
    }
