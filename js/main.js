7024f9eb7fd267973f1c946fd50f39a0602fbc02
ASHISH KUMAt <akumaraman966@gmail.com>
2022-09-14 16:13:34 +0530

    update
    update

--- js/main.js
+++ js/main.js
@@ -1,9 +1,25 @@
 
-// let junrate randum number 
+//=========getting input value=============
 
- const imageUrl ="https://picsum.photos/200/300?random="; 
+ var number =0;
 
-   for(let i=0; i<100; i++){
+   const input = document.getElementsByTagName('input');
+    let value = input[0].value;
+   input[1].addEventListener('click', ()=>{
+         let inputval = input[0].value;
+         number= number+input;
+   } )
+  
+ 
+
+    console.log(number);
+
+
+
+
+ 
+   const imageUrl ="https://picsum.photos/200/300?random="; 
+   for(let i=0; i<10; i++){
       let url =imageUrl +i;
       const row = document.createElement("div");
       row.setAttribute("class" ,"row");
@@ -24,6 +40,33 @@
 
       document.getElementById('main').appendChild(row);
    }
+ 
+
+
+// let junrate randum number 
+
+//  const imageUrl ="https://picsum.photos/200/300?random="; 
+//    for(let i=0; i<100; i++){
+//       let url =imageUrl +i;
+//       const row = document.createElement("div");
+//       row.setAttribute("class" ,"row");
+      
+//        const img = document.createElement("img");
+//            img.setAttribute("src" , url);
+//            row.append(img)
+     
+//        const h2 = document.createElement('h2'); 
+//         h2.innerText=`Image number ${i+1}`;
+      
+//         row.append(h2)
+//      // We can use this to add image discription;
+//       //   const p = document.createElement('p'); 
+//       //   p.innerText="this is a paragraf tax";
+      
+//       //   row.append(p)
+
+//       document.getElementById('main').appendChild(row);
+//    }
 
   
 
